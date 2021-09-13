bfefe359b273a98781dde89c334181ee910c5dbe
Bruno Almeida <brunokfsop@gmail.com>
2021-09-13 11:06:20 -0300

    feat: exercise 07 refactor

--- Exercises/07_exercise.py
+++ Exercises/07_exercise.py
@@ -1,17 +1,30 @@
-name = 'Bruno'
-age = 23
-height = 1.75
-weight = 67.5
-actual_year = 2021
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+height = float(input('What is your height? '))
+weight = float(input('What is your weight? '))
+current_year = int(input('What is the current year? '))
 
-def get_person_birth_year(age, actual_year):
-  return actual_year - age
+def get_person_birth_year(age, current_year):
+  return current_year - age
 
 def get_person_IMC(weight, height):
   return weight / height **2
 
 def get_person_info():
-  return f'Hello my name is {name}, I\'m {age} years old, actually I\'m {height} tall, i weigh {weight} KG and I was born in {get_person_birth_year(age, actual_year)}'
+  return f'Your name is {name}, you\'re {age} years old, actually you\'re {height} tall, weigh {weight} KG and were born in {get_person_birth_year(age=age, current_year=current_year)}'
+
+def get_IMC_rating(personIMC):
+  if (personIMC < 18.5):
+    return 'thinness'
+  elif (personIMC > 18.5 and personIMC < 25):
+    return 'regular'
+  elif (personIMC > 25 and personIMC < 30):
+    return 'overweight'
+  elif (personIMC > 30 and personIMC < 40):
+    return 'obesity'
+  else:
+    return 'severe obesity'
 
 print(get_person_info())
-print(f'IMC: {get_person_IMC(weight, height):.2f}')
+person_IMC = get_person_IMC(weight, height)
+print(f'Your current IMC is {get_IMC_rating(personIMC=person_IMC)}')
